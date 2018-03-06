05968218b9b7f54f44e2cbaeecc854b5a373d5df
Kevin DeLand <kevin.deland@gmail.com>
2018-03-06 14:45:36 -0500

    added "echo" commands to scripts

--- xprize.sh
+++ xprize.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 ## install needed APKs
 #adb install apk/RoboPolicy.xprize.apk
+echo 'Installing RTHomeScreen...'
 adb install apk/RoboLaunch.release.1.0.0.apk
+
+echo 'Installing FaceLogin...'
 adb install apk/facelogin.en.1.6.0.apk
+
+echo 'Installing RoboTutor...'
 adb install apk/robotutor.release_dbg.1.5.6.0.apk
 
 ## Set the device owner
+echo 'Setting Device Owner...'
 #adb shell dpm set-device-owner cmu.xprize.dpc/.AdminReceiver
 adb shell dpm set-device-owner cmu.xprize.rthomescreen/.AdminReceiver
 sleep 2
 
 ## Start the Activity
+echo 'Starting Activity...'
 #adb shell am start -n cmu.xprize.dpc/.LaunchActivity
 adb shell am start -n cmu.xprize.rthomescreen/.HomeActivity
--- xprize_uninstall.sh
+++ xprize_uninstall.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 ## Uninstall RoboTutor and FaceLogin
+echo 'Uninstalling RoboTutor...'
 adb uninstall cmu.xprize.robotutor
+echo 'Uninstalling FaceLogin...'
 adb uninstall com.example.iris.login1
 
 ## Run the backdoor service that removes device-owner privileges
 #adb shell am start -n cmu.xprize.dpc/cmu.xprize.dpc.RemoveAdmin
+echo 'Removing Device Owner privileges...'
 adb shell am start -n cmu.xprize.rthomescreen/cmu.xprize.rthomescreen.RemoveAdmin
 sleep 2
 
 ## uninstall package
 #adb uninstall cmu.xprize.dpc
+echo 'Uninstalling RTHomeScreen...'
 adb uninstall cmu.xprize.rthomescreen
