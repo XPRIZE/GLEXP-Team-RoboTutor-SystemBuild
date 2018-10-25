5f217f3d196c470701eed141b2a4c81bce77373c
Kevin DeLand <kevin.deland@gmail.com>
2018-10-25 11:10:18 -0400

    re-writing script for CodeDrop 2

--- assets/README.md
+++ assets/README.md
@@ -2,5 +2,24 @@
 
 This will hold the following files:
 
-* CodeDrop1_LitAudio.1.1.0.zip* CodeDrop1_LitStories.1.1.0.zip* CodeDrop1_NumberStories.1.1.0.zip* CodeDrop1_ReadingStories.1.1.0.zip* CodeDrop1_Songs.1.1.0.zip
-* RTAsset_Audio_SW_Set1.1.1.0.zip
+* CodeDrop1_LitAudio.1.1.0.zip
+* CodeDrop1_LitStories.1.1.0.zip
+* CodeDrop1_NumberStories.1.1.0.zip
+* CodeDrop1_Songs.1.1.0.zip
+* CodeDrop2_Original...
+* CodeDrop2_DemoVideos
+* CodeDrop2_Icons
+* CodeDrop2_MathStories
+* CodeDrop2_PuncStories
+* CodeDrop2_ReadingStories
+* CodeDrop2_Tutors
+
+
+#### How to consolidate?
+
+* [LitAudio] is really global... a bunch of words
+* Combine all extra stories, i.e. [LitStories, NumberStories, Songs, MathStories, PuncStories]
+* Combine all tutor assets, i.e. [CodeDrop2_Original, Tutors]
+* Keep ASB seperate [ReadingStories]
+* Keep DemoVids separate [DemoVideos]
+* Keep Icons separate [CodeDrop2_Icons]
--- xprize.sh
+++ xprize.sh
@@ -1,29 +1,49 @@
 #!/bin/sh
 
+DOWNLOAD_DIR = "/sdcard/Download"
+
+## TODO don't forget the "config.json" file
+echo "Pushing Config file..."
+adb push config_sw/config.json /sdcard/Download
+
 echo "Pushing Assets..."
-echo "Pushing Audio Assets..."
-# for testing... adb push assets/RTAsset_Audio_SW_Set1.1.1.0/assets /sdcard/robotutor_assets
-adb push assets/RTAsset_Audio_SW_Set1.1.1.0.zip /sdcard/Download
+echo "Pushing Tutor Assets..."
+adb push assets/CodeDrop2_Tutors.1.1.0.zip /sdcard/Download
+adb push assets/CodeDrop2_Original.1.1.0.zip /sdcard/Download
 
 
 # for testing... adb push assets/RTAsset_Stories_SW_Set1.1.1.0/assets /sdcard/robotutor_assets
-echo "Pushing LitAudio..." >&2
+echo "Pushing Global Words Audio..." >&2
 adb push assets/CodeDrop1_LitAudio.1.1.0.zip /sdcard/Download
+
+## Pushing Tutor Assets
+echo "Pushing Tutor Assets..." >&2
+adb push assets/CodeDrop2_Tutors.1.1.0.zip /sdcard/Download
+echo "Pushing More Tutor Assets..." >&2
+adb push assets/CodeDrop2_Original.1.1.0.zip /sdcard/Download
+
+## small story collections
+echo "Pushing Small Story collections..." >&2
 echo "Pushing LitStories..." >&2
 adb push assets/CodeDrop1_LitStories.1.1.0.zip /sdcard/Download
 echo "Pushing NumberStories..." >&2
 adb push assets/CodeDrop1_NumberStories.1.1.0.zip /sdcard/Download
-echo "Pushing ReadingStories..." >&2
-adb push assets/CodeDrop1_ReadingStories.1.1.0.zip /sdcard/Download
 echo "Pushing Songs..." >&2
-adb push assets/CodeDrop1_Songs.1.1.0.zip /sdcard/Download    
+adb push assets/CodeDrop1_Songs.1.1.0.zip /sdcard/Download      
+echo "Pushing Word Problem Stories..." >&2
+adb push assets/CodeDrop2_MathStories.1.1.0.zip /sdcard/Download
+echo "Pushing Punctuation Stories..." >&2
+adb push assets/CodeDrop2_PuncStories.1.1.0.zip /sdcard/Download
+
+### African Story Book
+echo "Pushing ReadingStories..." >&2
+adb push assets/CodeDrop2_ReadingStories.1.1.0.zip /sdcard/Download
 
 ### create folder for demo_vids
 ### adb push demo_vids
 ## https://stackoverflow.com/questions/13373489/adb-shell-command-mkdir-for-creating-a-directory
-echo "Pushing demo videos..."
-adb push robotutor_assets  /sdcard/ 
-
+## echo "Pushing demo videos..."
+### adb push robotutor_assets  /sdcard/  ## TODO replace
 
 
 ## install needed APKs
@@ -37,8 +57,7 @@ adb install apk/RoboLaunch.xprize.1.0.0.apk
 echo 'Installing RoboTutor...'
 ## THIS MUST BE x.x.x.1 FOR XPRIZE!!!!!
 #for quicker testing...
-## adb install apk/robotutor.release_dbg.1.8.6.1.apk
-adb install apk/robotutor.release_sw.1.8.9.1.apk
+adb install apk/robotutor.release_dbg.2.7.2.1.apk       #### TODO Replace with most recent version
 
 echo 'Installing RoboTransfer...'
 adb install apk/RoboTransfer-xprize.apk
@@ -66,3 +85,6 @@ adb shell am start -n cmu.xprize.rthomescreen/.HomeActivity
 
 
 echo 'DONE!!! Please tap on the tablet screen and wait for the yellow bars to complete.'
+
+
+### TODO most important, make xprize.bat look *just like this one* and *test it*
