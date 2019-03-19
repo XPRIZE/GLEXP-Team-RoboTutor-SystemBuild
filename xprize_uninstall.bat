8584ece8bb31e80ea33ec87b75eded5a0ea9aa70
Prabhat Tyagi <prabhatt@maqsoftware.net>
2019-03-19 15:46:31 +0530

    Commented commands specific to Swahili version

--- xprize_uninstall.bat
+++ xprize_uninstall.bat
@@ -3,3 +3,25 @@ echo off
 :: Uninstall RoboTutor and FaceLogin (and RoboTransfer)
 echo 'Uninstalling RoboTutor...'
 adb uninstall cmu.xprize.robotutor
+
+:: Commented since these applications are not required for localization
+:: echo 'Uninstalling FaceLogin...'
+:: adb uninstall com.example.iris.login1
+
+:: echo 'Uninstalling RoboTransfer...'
+:: adb uninstall cmu.xprize.service_ftp
+
+:: Commented since these activities are not required for localization
+:: Run the backdoor service that removes device-owner privileges
+:: adb shell am start -n cmu.xprize.dpc/cmu.xprize.dpc.RemoveAdmin
+
+:: echo 'Removing Device Owner privileges...'
+:: adb shell am start -n cmu.xprize.rthomescreen/cmu.xprize.rthomescreen.RemoveAdmin
+
+:: ping -n 3 127.0.0.1>nul
+
+:: uninstall package
+:: adb uninstall cmu.xprize.dpc
+
+:: echo 'Uninstalling RTHomeScreen...'
+:: adb uninstall cmu.xprize.rthomescreen
--- xprize_uninstall.sh
+++ xprize_uninstall.sh
@@ -3,3 +3,21 @@
 ## Uninstall RoboTutor and FaceLogin
 echo 'Uninstalling RoboTutor...'
 adb uninstall cmu.xprize.robotutor
+
+# echo 'Uninstalling FaceLogin...'
+# adb uninstall com.example.iris.login1
+
+## echo 'Uninstalling RoboTransfer...'
+## adb uninstall cmu.xprize.service_ftp
+
+## Commented since these activities are not required for localization
+## Run the backdoor service that removes device-owner privileges
+## echo 'Removing Device Owner privileges...'
+## for testing... adb shell am start -n cmu.xprize.dpc/cmu.xprize.dpc.RemoveAdmin
+## adb shell am start -n cmu.xprize.rthomescreen/cmu.xprize.rthomescreen.RemoveAdmin
+## sleep 2
+
+## uninstall package
+## for testing... adb uninstall cmu.xprize.dpc
+## echo 'Uninstalling RTHomeScreen...'
+## adb uninstall cmu.xprize.rthomescreen
